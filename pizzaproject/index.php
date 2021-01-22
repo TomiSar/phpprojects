d8b680ce788446c5d5de71efe91ad2562231671d
Tomi Sarjamo <tomppa.sarjamo@gmail.com>
2021-01-22 14:37:16 +0200

    Redirection from Pizza application title to index.php. Updated index.php so that the data is received from pizzas Table. Form updated to more solid looking.

--- pizzaproject/index.php
+++ pizzaproject/index.php
@@ -8,23 +8,55 @@
 		echo 'Connection error: ' . mysqli_connect_error();
 	}
 
-	// write query for all pizzas title, ingredients, id rows from pizzas Table
-	$sql = 'SELECT title, ingredients, id FROM pizzas';
+	// write query for all pizzas title, ingredients, id rows from pizzas Table order by creation_time;
+	$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 	$result = mysqli_query($conn, $sql);			   // make query and get result mysqli_query(connection, sql_query);
 	$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC); // fetch the resulting rows as an array
 	mysqli_free_result($result); 					   // free result from memory
 	mysqli_close($conn); 		 					   // close connection
-
-	print_r($pizzas);
+	//explode(',', $pizzas[0]['ingredients']);
+	//print_r($pizzas);
 ?>
 
 <!DOCTYPE html>
 <html>
 
-	<?php include('templates/header.php') ?>
-	<?php include('templates/footer.php') ?>
+	<?php include('templates/header.php'); ?>
 
-<body>
+	<h4 class="center grey-text">Pizzas!</h4>
 
-</body>
+	<div class="container">
+		<div class="row">
+
+			<?php foreach($pizzas as $pizza): ?>
+				
+				<div class="col s6 md3">
+				  <div class="card z-depth-0">
+				 	<div class="card-content center">
+				 		<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+				 		<ul>
+				 			<?php foreach(explode(',', $pizza['ingredients']) as $ingredient): ?>
+				 				<li><?php echo htmlspecialchars($ingredient); ?></li>
+				 			<?php endforeach; ?>
+				 		</ul>
+				 	</div>
+				 	<div class="card-action right-align">
+				 		<a class="brand-text" href="#">More info</a>
+				 	</div>
+				  </div>					
+				</div>
+
+			<?php endforeach; ?>
+
+
+			<?php if(count($pizzas) >= 3): ?>
+				<p>There are three or more pizzas</p>
+			<?php else: ?>
+				<p>There are less than three pizzas</p>
+			<?php endif; ?>
+
+		</div>
+	</div>
+	<?php include('templates/footer.php'); ?>
+
 </html>
--- pizzaproject/templates/header.php
+++ pizzaproject/templates/header.php
@@ -20,9 +20,9 @@
 	<body class="grey lighten-4">
 		<nav class="white z-depth-0">
 			<div class="container">
-				<a href="#" class="brand-logo brand-text">Ninja Pizza</a>
+				<a href="index.php" class="brand-logo brand-text">Pizza application</a>    <!--Pizza application redirect to index.php-->
 				<ul id="nav-mobile" class="right hide-on-small-and-down">
-					<li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
+					<li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li> <!--Add a pizza redirect to add.php-->
 				</ul>
 			</div>
 		</nav>
